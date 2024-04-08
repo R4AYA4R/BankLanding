ad20d15b91b194020e778633a78fc9080b4a7beb
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-04-08 13:13:58 +0300

    added adaptive MenuMobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import cl from './Header.module.css';
-const Header=()=>{
-    return(
+import { useState } from 'react';
+const Header = () => {
+    const [isActiveMenu, setIsActiveMenu] = useState(false);
+    return (
         <header className={cl.header}>
             <div className="container">
                 <div className={cl.header__inner}>
@@ -11,15 +13,32 @@ const Header=()=>{
                     </NavLink>
                     <ul className={cl.header__menu}>
                         <li className="header__menu-item">
-                            <NavLink to="/" className={({isActive})=> isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link }>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link}>Home</NavLink>
                         </li>
                         <li className="header__menu-item">
                             <a href="#" className={cl.menu__link}>Careers</a>
                         </li>
                         <li className="header__menu-item">
-                            <NavLink to="/about" className={({isActive})=> isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link }>About</NavLink>
+                            <NavLink to="/about" className={({ isActive }) => isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link}>About</NavLink>
                         </li>
                     </ul>
+                    <ul className={isActiveMenu ? `${cl.header__menuMobile} ${cl.header__menuMobileActive}` : cl.header__menuMobile}>
+                        <li className="header__menu-item">
+                            <NavLink to="/" className={({ isActive }) => isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link} onClick={()=>setIsActiveMenu(false)}>Home</NavLink>
+                        </li>
+                        <li className="header__menu-item">
+                            <a href="#" className={cl.menu__link}>Careers</a>
+                        </li>
+                        <li className="header__menu-item">
+                            <NavLink to="/about" className={({ isActive }) => isActive ? `${cl.menu__link} ${cl.menu__linkActive}` : cl.menu__link} onClick={()=>setIsActiveMenu(false)}>About</NavLink>
+                        </li>
+                    </ul>
+                    <button className={cl.header__menuBtn} onClick={() => setIsActiveMenu((prev) => !prev)}>
+                        <span className={isActiveMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive1}` : cl.menuBtn__span}></span>
+                        <span className={isActiveMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive2}` : cl.menuBtn__span}></span>
+                        <span className={isActiveMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive3}` : cl.menuBtn__span}></span>
+                        <span className={isActiveMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive1}` : cl.menuBtn__span}></span>
+                    </button>
                 </div>
             </div>
         </header>
