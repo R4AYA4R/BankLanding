4078c94d029b3bffcc8aace338ae542435afc04c
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-04-09 17:10:48 +0300

    changed images github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="footer__inner">
                     <div className="footer__topline">
                         <Link to="/" className={cl.logo}>
-                            <img src="BankLanding/images/header/shape-30.png" alt="" className={cl.logo__img} />
+                            <img src="/BankLanding/images/header/shape-30.png" alt="" className={cl.logo__img} />
                             <p className={cl.logo__text}>YourBank</p>
                         </Link>
                         <ul className={cl.footer__menu}>
@@ -23,15 +23,15 @@ const Footer = () => {
                         </ul>
                         <div className="footer__topline-contacts">
                             <a href="#" className="contacts__email">
-                                <img src="BankLanding/images/footer/Icon.png" alt="" className="contacts__email-img" />
+                                <img src="/BankLanding/images/footer/Icon.png" alt="" className="contacts__email-img" />
                                 <p className="contacts__email-text">hello@skillbirdge.com</p>
                             </a>
                             <a href="#" className="contacts__tel">
-                                <img src="BankLanding/images/footer/Icon (1).png" alt="" className="contacts__tel-img" />
+                                <img src="/BankLanding/images/footer/Icon (1).png" alt="" className="contacts__tel-img" />
                                 <p className="contacts__tel-text">+91 91813 23 2309</p>
                             </a>
                             <a href="#" className="contacts__location">
-                                <img src="BankLanding/images/footer/Icon (2).png" alt="" className="contacts__location-img" />
+                                <img src="/BankLanding/images/footer/Icon (2).png" alt="" className="contacts__location-img" />
                                 <p className="contacts__location-text">Somewhere in the World</p>
                             </a>
                         </div>
@@ -39,13 +39,13 @@ const Footer = () => {
                     <div className="footer__bottomline">
                         <div className="footer__bottomline-images">
                             <a href="" className="footer__bottomline-link">
-                                <img src="BankLanding/images/footer/Button.png" alt="" className="footer__bottomline-img" />
+                                <img src="/BankLanding/images/footer/Button.png" alt="" className="footer__bottomline-img" />
                             </a>
                             <a href="" className="footer__bottomline-link">
-                                <img src="BankLanding/images/footer/Button (1).png" alt="" className="footer__bottomline-img" />
+                                <img src="/BankLanding/images/footer/Button (1).png" alt="" className="footer__bottomline-img" />
                             </a>
                             <a href="" className="footer__bottomline-link">
-                                <img src="BankLanding/images/footer/Button (2).png" alt="" className="footer__bottomline-img" />
+                                <img src="/BankLanding/images/footer/Button (2).png" alt="" className="footer__bottomline-img" />
                             </a>
                         </div>
                         <p className="footer__bottomline-text">YourBank All Rights Reserved</p>
